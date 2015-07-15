f2e37a5cd7baff374a9777c92864a91611f4230c
mindwing <mindwing@gmail.com>
2015-07-16 02:36:16 +0900

    .

---
+++ CallByValueTest.java
@@ -0,0 +1,23 @@
+public class CallByValueTest {
+	public static void m() {
+	}
+
+	public static void m(int i) {
+		i = 99;
+	}
+
+	public static void m(int[] arr) {
+		arr[0] = 99;
+	}
+
+	public static void main(String[] args) {
+		m();
+		int i = 10;
+		m(i);
+		System.out.println(i); // 결과예상
+
+		int[] arr = { 10, 20, 30 };
+		m(arr);
+		System.out.println(arr[0]); // 결과예상
+	}
+}
